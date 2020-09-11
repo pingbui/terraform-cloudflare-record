300ead8801eeca5a56f5b8088de898c1aded3391
thanhbn87 <thanhbn87@gmail.com>
2020-09-11 11:27:13 +0700

    Set default for api_token var

--- vars.tf
+++ vars.tf
@@ -1,8 +1,24 @@
-variable "cf_email" {}
-variable "cf_api_key" {}
-variable "cf_api_token" { default = "" }
+variable "cf_email" {
+  description = "The email associated with the account"
+  type        = string
+}
 
-variable "domain_name" {}
+variable "cf_api_key" {
+  description = "The Cloudflare API key"
+  type        = string
+}
+
+variable "cf_api_token" { 
+  description = "The Cloudflare API Token. This is an alternative to email+api_key. If both are specified, api_token will be used over email+api_key fields"
+  type        = string
+  default     = null
+}
+
+variable "domain_name" {
+  description = "The Domain name of these records"
+  type        = string
+}
+
 variable "records" {
   type    = list
   default = [
@@ -11,6 +27,7 @@ variable "records" {
 }
 
 variable "cf_ttl" {
-  type    = string
-  default = "1"
+  description = "The TTL of these records. Value of 1 is 'automatic'"
+  type        = string
+  default     = "1"
 }
